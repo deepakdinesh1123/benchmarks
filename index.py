fb8610151a6a7de9dc66c2b0898920cbc530ccf7
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-03-30 14:57:25 +0530

    flake8 errors fixed

--- index.py
+++ index.py
@@ -1,17 +1,16 @@
-from bs4 import BeautifulSoup
 import requests
 from lxml import etree
 from io import StringIO
 
 resp = requests.get('https://www.google.com/').text
-#xml = etree.fromstring(resp)
-#f = open('lxml_pretty.xml', 'w')
-#f.write(etree.tostring(xml, pretty_print=True))
+# xml = etree.fromstring(resp)
+# f = open('lxml_pretty.xml', 'w')
+# f.write(etree.tostring(xml, pretty_print=True))
 parser = etree.XMLParser(remove_blank_text=True)
 file_obj = StringIO(resp)
 tree = etree.parse(file_obj, parser)
 print(etree.tostring(tree, pretty_print=True))
-#soup = BeautifulSoup(resp, features="lxml")
-#pretty = soup.prettify().encode('utf-8').strip()
-#with open('pretty.xml', 'wb+') as fp:
+# soup = BeautifulSoup(resp, features="lxml")
+# pretty = soup.prettify().encode('utf-8').strip()
+# with open('pretty.xml', 'wb+') as fp:
 #    fp.write(pretty)
