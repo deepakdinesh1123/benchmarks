da60edb8d7f4cbb8a571fabc2a19967324ff7774
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-06-10 07:46:52 +0530

    test runs on multiple machines

--- benchmarks/models.py
+++ benchmarks/models.py
@@ -1,8 +1,7 @@
 import os
 
-from django.db import models
-
 import django
+from django.db import models
 
 try:
     os.environ["DJANGO_SETTINGS_MODULE"] = "benchmarks.settings"
@@ -16,22 +15,5 @@ class Book(models.Model):
     author = models.ForeignKey("Author", on_delete=models.CASCADE, null=True)
 
 
-class OneField(models.Model):
-    field1 = models.CharField(max_length=100)
-
-
 class Author(models.Model):
     author = models.CharField(max_length=100)
-
-
-class MultiField(models.Model):
-    field1 = models.CharField(max_length=100)
-    field2 = models.CharField(max_length=100)
-    field3 = models.CharField(max_length=100)
-    field4 = models.CharField(max_length=100)
-    field5 = models.CharField(max_length=100)
-    field6 = models.CharField(max_length=100)
-    field7 = models.CharField(max_length=100)
-    field8 = models.CharField(max_length=100)
-    field9 = models.CharField(max_length=100)
-    field10 = models.CharField(max_length=100)
