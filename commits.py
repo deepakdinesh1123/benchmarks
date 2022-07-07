d971ff5516734e481ba54bfa248e22557ebf0e14
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-07-07 21:42:39 +0530

    Build result history

---
+++ commits.py
@@ -0,0 +1,9 @@
+import subprocess
+
+with open("commits.txt", "r") as f:
+    a = f.read().splitlines()
+
+for i in range(len(a) - 1):
+    subprocess.call(
+        f"asv continuous --interleave-processes -a processes=2 --split --show-stderr {a[i]} {a[i+1]}"
+    )
