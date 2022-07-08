6010fb07960481e6813f6c34ea568493b0cfa097
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-07-08 06:48:19 +0530

    build history for the benchmarks

--- commits.py
+++ commits.py
@@ -4,6 +4,9 @@ with open("commits.txt", "r") as f:
     a = f.read().splitlines()
 
 for i in range(len(a) - 1):
-    subprocess.call(
-        f"asv continuous --interleave-processes -a processes=2 --split --show-stderr {a[i]} {a[i+1]}"
+    subprocess.run(
+        [
+            f"asv continuous --interleave-processes -a processes=2 --split --show-stderr {a[i]} {a[i+1]}"
+        ],
+        shell=True,
     )
